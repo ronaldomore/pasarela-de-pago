19186ca20cbb7b2147ac31a1dc172a95b84caa94
RonaldoMoreno <ronalmore118@gmail.com>
2022-11-22 00:02:13 -0600

    skeletop

---
+++ pasareladepago.php
@@ -0,0 +1,18 @@
+<?php
+/** 
+ *Plugin Name: Pasarela De Pago
+ *Plugin URI: https://ronalcomer.local/wp-admin/plugins.php
+ *Description: Plugin para realizar autorizacion y capturas con tarjeta de credito/debito
+ *Version: 1.0
+ *Author: Ronaldo Jose Peñalba Moreno.
+ *Author URI: https://ronalcomer.local/wp-admin/plugins.php
+ *License: JPLv2
+*/
+
+add_action('wp_footer', function(){?>
+
+<script>
+    alert('Hola mundo ');
+</script>
+
+<?php } ,9999); ?>
